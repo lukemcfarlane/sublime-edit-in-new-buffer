84d932f764a89dbc9c0fcde0cd3b103df98d448a
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-10-03 10:51:14 +1300

    Initial commit

---
+++ EditInNewBuffer.py
@@ -0,0 +1,32 @@
+import sublime, sublime_plugin, re, os, os.path
+
+class EditInNewBufferCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        if len(self.view.sel()) == 1:
+            sel = self.view.substr(self.view.word(self.view.sel()[0]))
+            window = self.view.window()
+            new_view = window.new_file()
+            window.focus_view(new_view)
+            new_view.run_command("append", {"characters": sel });
+            settings = sublime.load_settings("EditInNewBuffer.sublime-settings")
+            settings.set("currently_editing_view_id", self.view.id());
+            settings.set("active_view_id", new_view.id());
+        else:
+            print("Error: No text highlighted")
+
+class EditBufferEvent(sublime_plugin.EventListener):
+    def on_modified(self, view):
+        settings = sublime.load_settings("EditInNewBuffer.sublime-settings")
+        target_view_id = settings.get("currently_editing_view_id")
+        active_view_id = settings.get("active_view_id")
+        if view.id() == active_view_id:
+            all_views = view.window().views()
+            target_view = None
+            for v in all_views:
+                if(v.id() == target_view_id):
+                    target_view = v
+
+            if target_view == None:
+                print("Could not find target view with id: " + str(target_view_id))
+            else:
+                target_view.run_command("append", {"characters": "TEST APPEND"})
---
+++ README.md
@@ -0,0 +1,20 @@
+Edit In New Buffer
+================== 
+A Sublime Text 3 plugin.
+-------------------------
+
+1) Select some text in a tab/view
+2) Cmd + Shift + P -> Edit in new buffer
+3) Selected text is copied into a new buffer in a new tab
+4) Any changes made to the text in the new buffer is saved back to the original buffer as you type
+
+
+Installation
+------------
+
+Copy the following files into a new folder "EditInNewBuffer" inside your Sublime Packages directory:
+- EditInNewBuffer.py
+- Default.sublime-commands
+
+**Note:** to locate your Sublime Packages directory, from within Sublime Text go to the Sublime Text menu -> Preferences -> Browse Packages.
+
